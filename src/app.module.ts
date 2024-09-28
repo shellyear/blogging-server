4a4f2e8b9fe3e2c59111a93372a3ac5f04b0b5ed
shellyear <boolevafunkce@gmail.com>
2024-09-28 13:10:03 +0200

    [chore] delete unsused files, setup postgres connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,24 +1,20 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ArticleModule } from './article/article.module';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
-      type: 'mysql',
+      type: 'postgres',
       host: 'localhost',
-      port: 3306,
-      username: 'root',
-      password: 'root',
-      database: 'blog.db',
+      port: 5432,
+      username: 'nurbek',
+      password: '',
+      database: 'blog_db',
       autoLoadEntities: true, // load entities automatically from provided module
       synchronize: true,
     }),
     ArticleModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
